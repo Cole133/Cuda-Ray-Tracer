1d28a40bc988bc4c2d01cbfb8b9e460ead98c287
Cole Gardiner <colegardiner33@gmail.com>
2026-05-31 11:17:44 -0400

    Added depth of field

--- include/material.h
+++ include/material.h
@@ -67,7 +67,7 @@ class dielectric : public material {
             bool cannot_refract = ri * sin_theta > 1.0;
             vec3 direction;
 
-            if(cannot_refract)
+            if(cannot_refract || reflectance(cos_theta, ri) > random_double())
                 direction = reflect(unit_direction, rec.normal);
             else
                 direction = refract(unit_direction, rec.normal, ri);
@@ -79,5 +79,11 @@ class dielectric : public material {
     private:
     
         double refraction_index;
+
+        static double reflectance(double cosine, double ref_inx){
+            auto r0 = (1-ref_inx) / (1+ref_inx);
+            r0 = r0*r0;
+            return r0 + (1-r0)*std::pow((1 - cosine),5);
+        }
 };
 #endif
